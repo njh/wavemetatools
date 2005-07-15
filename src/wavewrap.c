aefc47dd0ea0cc6d405066288f6151ec33388360
Nicholas J Humfrey <njh@aelius.com>
2005-07-15 10:04:35 +0000

    very start

--- src/wavewrap.c
+++ src/wavewrap.c
@@ -31,13 +31,73 @@
 #include "util.h"
 
 
+void
+handle_error(char* errStr)
+{
+    fprintf(stderr, errStr);
+    //if (errNum) fprintf(stderr, "\n\t%x\n", errNum);
+    
+    exit(2);
+}
+
+
+/* Display how to use this program */
+static int usage( const char * progname )
+{
+    fprintf(stderr, "Wave Meta Tools version %s\n", VERSION);
+    fprintf(stderr, "Usage: %s <input> <output.wav>\n\n", progname);
+    exit(1);
+}
+
 
 int
 main(int argc, char **argv)
 {
+    u_int32_t seek = 0;
+    struct stat fileInfo;
+    FILE * input = NULL;
+	FILE * output = NULL;
+    char * inputname = NULL;
+    char * outputname = NULL;
+    int opt;
+    
+    while ((opt = getopt(argc, argv, "h")) != -1) {
+        switch (opt) {
+            default:
+                fprintf(stderr, "Unknown option '%c'.\n", (char)opt);
+            case 'h':
+                usage( argv[0] );
+                break;
+        }
+    }
 
-	printf("Echooooooo !\n");
+    if (argc-optind!=2) usage( argv[0] );
+    
+
+    // Initialise the globals
+    inputname = argv[optind];
+    outputname = argv[optind+1];
+
+    // Get the length of the file
+    if(stat(inputname, &fileInfo))   handle_error("Error: unable to stat file\n");
+
+    // Open the input file
+    input = fopen(inputname, "r");
+    if (input==NULL) handle_error("Error: unable to open input file\n");
+
+    // Open the output file
+    output = fopen(outputname, "w");
+    if (output==NULL) handle_error("Error: unable to open output file\n");
+
+
+	
+
+
+    // Close the file
+    fclose(input);
+    fclose(output);
     
     // Success !
     return 0;
 }
+
